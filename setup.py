55b502e1a982bf7cde331f62136495384c4b97de
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-10-20 17:10:17 +0200

    On branch master  Now the script in bin is actually installed  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   MANIFEST.in
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
+"""Setup script."""
 from setuptools import setup
 
 
 setup(name="Nordic44",
-        version="0.1",
-        description="Python package for generating N44 cases",
-        packages=["nordic44"],
-        package_data={"nordic44": ["models/*sav"]},
-        include_package_data=True,
-        scripts=['bin/data_from_nordpool.py'],
-        )
-
+      version="0.1",
+      description="Python package for generating N44 cases",
+      packages=["nordic44"],
+      package_data={"nordic44": ["models/*sav"]},
+      include_package_data=True,
+      scripts=['bin/data_from_nordpool.py'],)
