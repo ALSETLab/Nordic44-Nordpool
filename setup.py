38d96d32bca39aa0d5401e0bc6d5cd8c8d245b85
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-10-20 14:44:20 +0200

    On branch master  Forgot to add the dependencies of the excel example  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   MANIFEST.in
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(name="Nordic44",
         version="0.1",
         description="Python package for generating N44 cases",
         packages=["nordic44", "examples"],
-        package_data={"nordic44": ["models/*sav"]},
+        package_data={"nordic44": ["models/*sav"],
+            "examples": ["N44_20150101/*xlsx"]},
         include_package_data=True,
         )
 
